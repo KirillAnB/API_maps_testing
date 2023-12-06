6c047ffbe5f1671e47a65e03f3abd73ede136615
KirillAnB <it-reso@mail.ru>
2023-12-06 14:02:47 +0300

    test class added

---
+++ with_class.py
@@ -0,0 +1,22 @@
+import requests
+import pytest
+
+
+base_url = "https://api.chucknorris.io/jokes/random"
+fields = ['id', 'value', 'icon_url', 'url', 'categories']
+
+@pytest.fixture
+def get_joke():
+    data = requests.get(base_url)
+    yield data
+
+# def test_status_code(get_joke):
+#     assert get_joke.status_code == 200
+
+class Test_jokes():
+    def test_status_code(self,get_joke):
+        assert get_joke.status_code == 200
+
+    def test_fields(self, get_joke):
+        for field in fields:
+            assert field in get_joke.json()
